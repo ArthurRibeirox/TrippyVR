0c8380f0d9cf5649c6a55dbe7dea8695d4e273f5
ManoStuart <arthhur.ribeiro@gmail.com>
2017-02-20 01:28:54 -0300

    Finished renderTexture. Added "W H E R E   A M   I   G O I N G ?" video.

---
+++ Assets/Scripts/AutoplayMovie.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoplayMovie : MonoBehaviour {
+
+	void Awake () {
+		((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+	}
+}
